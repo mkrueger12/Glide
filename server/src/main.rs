3b446e744e9012925105fa582403e2ede3210493
Max Krueger <max.krueger@cyberdefense.global>
2023-11-20 13:46:21 -0700

    init

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,30 @@
 #![deny(warnings)]
+
+use futures::{FutureExt, StreamExt};
 use warp::Filter;
+
+// curl --include --no-sessionid --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Host: localhost:3030" --header "Origin: http://localhost:3030" --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" --header "Sec-WebSocket-Version: 13" --url http://localhost:3030/echo
+
 
 #[tokio::main]
 async fn main() {
-    // Match any request and return hello world!
-    let routes = warp::any().map(|| "Hello, World!");
+    pretty_env_logger::init();
+
+    let routes = warp::path("echo")
+        // The `ws()` filter will prepare the Websocket handshake.
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            // And then our closure will be called when it completes...
+            ws.on_upgrade(|websocket| {
+                // Just echo all messages back...
+                let (tx, rx) = websocket.split();
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error: {:?}", e);
+                    }
+                })
+            })
+        });
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
